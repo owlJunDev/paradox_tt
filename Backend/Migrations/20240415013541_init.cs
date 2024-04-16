5fb01d17190a3a92322f432a57dcc2c205e36ce3
owlJunDev <anime_i_game@mail.ru>
2024-04-16 17:04:38 +0500

    upload migration

---
+++ Backend/Migrations/20240415013541_init.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "note",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    title = table.Column<string>(type: "text", nullable: true),
+                    content = table.Column<string>(type: "text", nullable: true),
+                    datecreate = table.Column<DateTime>(name: "date-create", type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_note", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tag",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    nametag = table.Column<string>(name: "name_tag", type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tag", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NoteTag",
+                columns: table => new
+                {
+                    notesid = table.Column<Guid>(type: "uuid", nullable: false),
+                    tagsid = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NoteTag", x => new { x.notesid, x.tagsid });
+                    table.ForeignKey(
+                        name: "FK_NoteTag_note_notesid",
+                        column: x => x.notesid,
+                        principalTable: "note",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_NoteTag_tag_tagsid",
+                        column: x => x.tagsid,
+                        principalTable: "tag",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NoteTag_tagsid",
+                table: "NoteTag",
+                column: "tagsid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NoteTag");
+
+            migrationBuilder.DropTable(
+                name: "note");
+
+            migrationBuilder.DropTable(
+                name: "tag");
+        }
+    }
+}
